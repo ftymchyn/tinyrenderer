1a6e595d4366f1be7606c3387d9c2d41c9ef1d37
ftymchyn <fazzan.07@gmail.com>
2019-04-14 16:17:11 +0300

    fixed SEGV crash

--- srcs/render/set_pixel_color.c
+++ srcs/render/set_pixel_color.c
@@ -5,5 +5,5 @@ void	set_pixel_color(int x, int y, int c)
 	t_sdl	*sdl;
 
 	sdl = get_sdl_context();
-	sdl->pixels[(sdl->height - y) * sdl->width + x] = c;
+	sdl->pixels[(sdl->height - y - 1) * sdl->width + x] = c;
 }

bd2ef28aebaf3ab1b7c2d0efda7174feaca78914
ftymchyn <fazzan.07@gmail.com>
2019-04-14 16:13:51 +0300

    added typedefs t_float3 & t_int3

--- includes/common.h
+++ includes/common.h
@@ -6,7 +6,9 @@
 
 typedef unsigned char	t_byte4 __attribute__((ext_vector_type(4)));
 typedef float			t_float4 __attribute__((ext_vector_type(4)));
+typedef float			t_float3 __attribute__((ext_vector_type(4)));
 typedef int				t_int2 __attribute__((ext_vector_type(2)));
+typedef int				t_int3 __attribute__((ext_vector_type(3)));
 
 typedef enum			e_bool
 {

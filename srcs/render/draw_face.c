2143b43b00cf106e14b10d6f863c13309ddf7fed
ftymchyn <fazzan.07@gmail.com>
2019-04-15 07:16:36 +0300

    refactored draw_face

--- srcs/render/draw_face.c
+++ srcs/render/draw_face.c
@@ -1,27 +1,46 @@
 #include "render.h"
 #include <stdio.h>
 
-void	draw_face(t_face *face)
+static void	draw_triangle(t_int2 *points)
 {
-	t_sdl	*sdl;
 	t_color	c;
-	size_t	i;
 
-	if (face && vector_size(face->vdata) >= 3)
+	c.rgba = rand();
+	draw_line(points[0], points[1], c);
+	draw_line(points[1], points[2], c);
+	draw_line(points[2], points[0], c);
+}
+
+t_int2		transform(t_face *face, size_t vindex)
+{
+	t_int2			result;
+	const t_sdl		*sdl;
+	const t_int3	*vdata;
+	const t_float3	*v;
+
+	sdl = get_sdl_context();
+	vdata = vector_at(face->vdata, vindex);
+	v = vector_at(face->owner->vertexes, vdata->x - 1);
+	result = (t_int2){(v->x + 1.0f) * sdl->width / 2.0f, (v->y + 1.0f) * sdl->height / 2.0f };
+	return (result);
+}
+
+void		draw_face(t_face *face)
+{
+	t_int2		screen_coords[3];
+	size_t		vsize;
+	size_t		i;
+
+	vsize = vector_size(face->vdata);
+	if (face && vsize >= 3)
 	{
-		c.rgba = rand();
-		sdl = get_sdl_context();
-		i = 0;
-		while (i < 3)
+		screen_coords[0] = transform(face, 0);
+		i = 1;
+		while (i < vsize - 1)
 		{
-			t_int3		*vdata0 = vector_at(face->vdata, i);
-			t_int3		*vdata1 = vector_at(face->vdata, ((i + 1) % 3));
-			t_float3	*v0 = vector_at(face->owner->vertexes, vdata0->x - 1);
-			t_float3	*v1 = vector_at(face->owner->vertexes, vdata1->x - 1);
-			t_int2		p0 = (t_int2){(v0->x + 1.0f) * sdl->width / 2.0f, (v0->y + 1.0f) * sdl->height / 2.0f };
-			t_int2		p1 = (t_int2){(v1->x + 1.0f) * sdl->width / 2.0f, (v1->y + 1.0f) * sdl->height / 2.0f };
-
-			draw_line(p0, p1, c);
+			screen_coords[1] = transform(face, i);
+			screen_coords[2] = transform(face, i + 1);
+			draw_triangle(screen_coords);
 			i++;
 		}
 	}
